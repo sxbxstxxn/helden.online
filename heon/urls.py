c23fa206d5f6f57c7bacdb45a12ba5aa5a4a569e
sxbxstxxn <sxbxstxxn@googlemail.com>
2026-05-26 20:32:46 +0200

    sdaasd

--- heon/urls.py
+++ heon/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +29,6 @@ handler404 = 'web.views.not_found'
 handler403 = 'web.views.permission_denied'
 
 if settings.SERVE_MEDIA_FILES:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
